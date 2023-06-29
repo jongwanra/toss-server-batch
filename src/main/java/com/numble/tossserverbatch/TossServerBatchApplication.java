7e5bccc3c4874eb1920f54d299a7a7bc3bb07871
jongwanra <najongsi@gmail.com>
2023-06-29 18:08:58 +0900

    init setting & apply H2 Database

---
+++ src/main/java/com/numble/tossserverbatch/TossServerBatchApplication.java
@@ -0,0 +1,14 @@
+package com.numble.tossserverbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TossServerBatchApplication {
+
+	public static void main(String[] args) {
+		System.out.println("run start toss-server-batch");
+		SpringApplication.run(TossServerBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/numble/tossserverbatch/TossServerBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numble.tossserverbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TossServerBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
