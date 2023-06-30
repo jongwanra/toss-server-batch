2a68b65533e87645d6169219c33f6c94bc6c0e04
jongwanra <najongsi@gmail.com>
2023-06-30 15:11:26 +0900

    add MemberServiceImpl

--- src/main/java/com/numble/tossserverbatch/service/MemberService.java
+++ src/main/java/com/numble/tossserverbatch/service/MemberService.java
@@ -1,42 +1,7 @@
 package com.numble.tossserverbatch.service;
 
-
 import com.numble.tossserverbatch.api.request.MemberSignUpRequestDto;
-import com.numble.tossserverbatch.domain.Member;
-import com.numble.tossserverbatch.repository.MemberRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
-@Service
-@Transactional
-@RequiredArgsConstructor
-public class MemberService {
-    private final MemberRepository memberRepository;
-
-
-    public Long signUp (
-        MemberSignUpRequestDto request
-    ){
-        validateDulicatedMemberByName(request.getName());
-        Member createdMember = Member.withNameAndBirthDay(request.getName(), request.getBirthDay());
-        return memberRepository.save(createdMember).getId();
-    }
-
-    public void deleteMember () {
-        // 삭제할 수 있는지 상황인지 체크
-
-        // 삭제
-    }
-
-    private void validateDulicatedMemberByName(String name) {
-        List<Member> findMembers = memberRepository.findByName(name);
-
-        if(!findMembers.isEmpty()){
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
-        }
-    }
-
+public interface MemberService {
+    Long signUp(MemberSignUpRequestDto request);
 }
